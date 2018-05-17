2a562d65c9d9b522d2d4162f99b6ea404d65e66d
TheShahriyar <shahriyar.31dec@gmail.com>
2018-05-17 11:19:32 +0600

    File Upload

---
+++ assets/js/sidenav.js
@@ -0,0 +1,36 @@
+(function () {
+    
+    "use strict";
+	var bodyEl = document.body,
+		content = document.querySelector('.content-wrap'),
+		openbtn = document.getElementById('open-button'),
+		closebtn = document.getElementById('close-button'),
+		isOpen = false;
+
+	function init() {
+		initEvents();
+	}
+
+	function initEvents() {
+		openbtn.addEventListener('click', toggleMenu);
+		if (closebtn) {
+			closebtn.addEventListener('click', toggleMenu);
+		}
+
+		
+	}
+
+	function toggleMenu() {
+		if (isOpen) {
+			classie.remove(bodyEl, 'show-menu');
+		}
+		    else {
+                classie.add(bodyEl, 'show-menu');
+		    }
+        
+		isOpen = !isOpen;
+	}
+
+	init();
+
+})();
